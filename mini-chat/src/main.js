19ee8af0f3d3c11530bd2dfb44a31a86a6e93885
rayn <lamlaikarayane@gmail.com>
2026-08-19 17:16:25 +0200

    re setup everything end to end

--- mini-chat/src/main.js
+++ mini-chat/src/main.js
@@ -46,6 +46,7 @@ async function signup(req, res) {
                 message: "Email already exists"
             });
         }
+        
         const result = await pool.query(
             `INSERT INTO users
                 (username, email, password_hash)
